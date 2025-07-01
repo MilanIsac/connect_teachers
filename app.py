1d8f394a8e751ca741ceb32880b68b57cb1812a1
Milan Isac <milan.isac.7@gmail.com>
2025-07-01 21:16:32 +0530

    styling added

--- app.py
+++ app.py
@@ -44,7 +44,11 @@ def create_app():
     app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
     app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
     app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
-    mysql.init_app(app)
+    # mysql.init_app(app)
+    try:
+        mysql.init_app(app)
+    except Exception as e:
+        print("❌ MySQL init error:", e)
 
     # Cloudinary configuration
     cloudinary.config(
